f8a324a3468b21e020829a94e9a5bafc1d0af9ad
Joel Nichols <me@joelnichols.co>
2013-08-05 16:10:22 +0100

    Staff group adding

--- app/Controller/StaffGroupsController.php
+++ app/Controller/StaffGroupsController.php
@@ -12,4 +12,24 @@ class StaffGroupsController extends AppController {
 
 		$this->set('group', $group);
 	}
+
+	public function add() {
+		$Authentication = new Authentication;
+
+		$cmsuser = $this->CmsUser->findByUser($Authentication->Username());
+		if ($cmsuser == null) {
+			$this->redirect(array('controller' => 'CmsUsers', 'action' => 'accessdenied'));
+		}
+		$this->set('title', 'Add New Staff Group');
+		if ($this->request->is('post')) {
+			if ($this->StaffGroup->save($this->request->data)) {
+				$this->Session->setFlash('
+					<div class="alert alert-success">
+						<button class="close" data-dismiss="alert">&times;</button>
+						Staff group saved successfully.
+					</div>
+				');
+			}
+		}
+	}
 }
