5be01474b2fff7ff75053c95118b47e178c9d2a6
Joel Nichols <me@joelnichols.co.uk>
2013-09-23 12:45:39 +0100

    Update

--- app/View/Handbook/home.ctp
+++
@@ -1,2 +0,0 @@
-<h2>Welcome</h2>
-<p>Welcome to the Esher High School Staff Handbook. Please use the menu on the left to navigate your way around the staff handbook.</p>
--- app/webroot/handbook/manage/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-include "../connect.php";
-include "../access.php";
-require_pub_or_admin();
-include "header.php"; ?>
-<h3>Welcome</h3>
-Welcome to the Staff Handbook Mangement area, please select a category from above to begin.
-<?php include "footer.php"; ?>
